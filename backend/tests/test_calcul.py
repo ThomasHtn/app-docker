2b7e13905c704fbe93fceed66b5cba28806723a4
Thomas <thomas.houtin@solicis.fr>
2025-07-01 17:33:39 +0200

    fix(path): Fix test path

--- backend/tests/test_calcul.py
+++ backend/tests/test_calcul.py
@@ -1,5 +1,10 @@
-import pytest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from modules.calcul import compute_square
+
 
 def test_compute_square():
     assert compute_square(3) == 9
